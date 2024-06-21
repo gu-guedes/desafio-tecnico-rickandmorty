055e27db093f942621a732c75bec54b936aadb1e
unknown <guguedes2009@gmail.com>
2024-06-21 01:24:02 -0300

    feat: creating header

---
+++ src/app/components/header.js
@@ -0,0 +1,21 @@
+"use client"
+import { styled } from "styled-components"
+import { SearchInput } from "./search-input"
+
+
+export function Header() {
+    return (
+        <TagHeader>
+<h1>Rick and Morty</h1>
+<SearchInput></SearchInput>
+        </TagHeader>
+    )
+}
+
+const TagHeader = styled.header`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 20px 160px;
+background-color: red;
+`
---
+++ src/app/components/search-input.js
@@ -0,0 +1,36 @@
+
+import { styled } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export function SearchInput() {
+  return (
+    <SearchContainer>
+      <StyledInput type="text" placeholder="Pesquisar..." />
+      <SearchIcon icon={faSearch} />
+    </SearchContainer>
+  );
+}
+
+const SearchContainer = styled.div`
+  position: relative;
+  width: 350px;
+`;
+
+const StyledInput = styled.input`
+  width: 100%;
+  border-radius: 10px;
+  background-color: white;
+  padding: 10px 15px 10px 40px; 
+  border: 1px solid #ccc;
+  font-size: 16px;
+`;
+
+const SearchIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  color: #ccc;
+  pointer-events: none; 
+`;
