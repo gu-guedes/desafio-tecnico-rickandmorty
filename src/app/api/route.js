6b5452444ef7bfe6d413b3972cfec7f1163c9b53
unknown <guguedes2009@gmail.com>
2024-06-24 17:50:07 -0300

    Update .gitignore and refactor API routes to use environment variables

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://rickandmortyapi.com/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getCharacters = async (page, name) => {
   try {
